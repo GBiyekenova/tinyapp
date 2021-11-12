c5e611a97efcea0ed307165121b3d499aadf32e7
Gulbike Biyekenova <gbknva@gmail.com>
2021-11-12 21:45:02 +0000

    exports functions and objects

--- helpers.js
+++ helpers.js
@@ -11,4 +11,49 @@ const getUserByEmail = (email, database) => {
   return result;
 };
 
-module.exports = { getUserByEmail };
+const urlsForUser = (userID, database) => {
+    const urls = {};
+    let arrOfKeys = Object.keys(database);
+    for (const key of arrOfKeys) {
+      if (database[key].userID === userID) {
+        urls[key] = database[key].longURL;
+      }
+    }
+    return urls;
+  };
+
+  function generateRandomString(length) {
+    let result = "";
+    let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let charsLength = chars.length;
+    for (let i = 0; i < length; i++) {
+      result += chars.charAt(Math.floor(Math.random() * charsLength));
+    }
+    return result;
+  }
+
+  const users = {
+    "userRandomID": {
+      id: "userRandomID",
+      email: "user@example.com",
+      password: "purple-monkey-dinosaur"
+    },
+    "user2RandomID": {
+      id: "user2RandomID",
+      email: "user2@example.com",
+      password: "dishwasher-funk"
+    }
+  };
+
+  const urlDatabase = {
+    b6UTxQ: {
+      longURL: "https://www.tsn.ca",
+      userID: "aJ48lW"
+    },
+    i3BoGr: {
+      longURL: "https://www.google.ca",
+      userID: "aJ48lW"
+    }
+  };
+
+module.exports = { getUserByEmail, urlsForUser, generateRandomString, users, urlDatabase };
